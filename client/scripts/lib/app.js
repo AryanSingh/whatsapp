35f6a21073cc6a77a38b47f4028b1948a68b8787
aryan singh <aryan@iitgn.ac.in>
2016-06-15 13:18:22 +0530

    step 3 complete

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -1,6 +1,7 @@
 // Libs
 import 'angular-animate';
 import 'angular-meteor';
+import 'angular-moment';
 import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
@@ -13,18 +14,23 @@ import { Meteor } from 'meteor/meteor';
 import CalendarFilter from '../filters/calendar.filter';
 import RoutesConfig from '../routes';
 import ChatsCtrl from '../controllers/chats.controller';
+import ChatCtrl from '../controllers/chat.controller';
+import InputDirective from '../directives/input.directive';
  
 const App = 'Whatsapp';
  
 // App
 Angular.module(App, [
   'angular-meteor',
+  'angularMoment',
   'ionic'
 ]);
 
 new Loader(App)
   .load(RoutesConfig)
   .load(ChatsCtrl)
+  .load(ChatCtrl)
+  .load(InputDirective)
   .load(CalendarFilter);
  
 // Startup
